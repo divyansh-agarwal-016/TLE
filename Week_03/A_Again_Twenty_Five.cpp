af0829a75ef43f93ce38672a7ca69f3896caf125
Divyansh Agarwal <divyanshagarwal026@gmail.com>
2026-01-22 08:10:22 +0000

    Week 03 Day 02 Problem 01

---
+++ Week_03/A_Again_Twenty_Five.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/630/A
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if (n == 1){
+        cout << 5 << endl;
+    }
+    else{
+        cout << 25 << endl;
+    }
+}
